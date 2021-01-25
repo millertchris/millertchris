5c03e5c784d56b8b2bac282a198130b20598e795
Chris Miller <miller.t.chris@gmail.com>
2021-01-24 20:19:23 -0500

    Major overhaul, added autloader, converted functions to objects, purged, restructured

--- functions/wp/_base.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once dirname(__FILE__) . '/api.php';
-    require_once dirname(__FILE__) . '/admin.php';
-    require_once dirname(__FILE__) . '/theme-resources.php';
-    require_once dirname(__FILE__) . '/theme-support.php';
-    require_once dirname(__FILE__) . '/tweaks.php';
--- header.php
+++ header.php
@@ -1,29 +1,12 @@
 <!doctype html>
-<?php $site_url = site_url(); ?>
-<?php $template_directory = get_template_directory_uri() . '/favicon.ico'; ?>
-
-
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
+<?php if (is_search()) { ?>
+    <meta name="robots" content="noindex, nofollow">
+<?php } ?>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
+<?php wp_head(); ?>
 
-	<?php if (is_search()) { ?>
-		<meta name="robots" content="noindex, nofollow">
-	<?php } ?>
-
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
-
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
-
-	<?php wp_head(); ?>
-
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/static/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/static/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/static/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/static/favicon/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/static/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body <?php body_class('body'); ?>>
