492a84a2b89aaaa4a06a93fc910f4a4fa4212cec
Chris Miller <miller.t.chris@gmail.com>
2022-01-17 21:41:13 -0500

    Added links to the portfolio page.

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,63 +5,165 @@ import Header from '../components/header';
 import Tilt from 'react-parallax-tilt';
 
 const items = [
-  { title: 'Old Forester', image: 'old-forester-website.jpeg' },
+  {
+    title: 'Old Forester',
+    image: 'old-forester-website.jpeg',
+    url: 'https://www.oldforester.com/',
+  },
   { title: 'Buckhead', image: 'buckheads-website.png' },
-  { title: 'Southern Comfort', image: 'southern-comfort-website.jpeg' },
-  { title: 'Metro United Way', image: 'metro-united-way-website.png' },
+  {
+    title: 'Southern Comfort',
+    image: 'southern-comfort-website.jpeg',
+    url: 'http://originalsoutherncomfort.com/en-us/',
+  },
+  {
+    title: 'Metro United Way',
+    image: 'metro-united-way-website.png',
+    url: 'https://metrounitedway.org/',
+  },
   { title: 'Level', image: 'level-branding.jpg' },
-  { title: 'Schedule It', image: 'schedule-it-website.jpg' },
-  { title: 'Goldhouse', image: 'goldhouse-website.jpg' },
-  { title: 'Frost Brown Todd', image: 'frost-brown-todd-website.png' },
-  { title: 'Elegance Living', image: 'elegance-living-website.png' },
-  { title: 'Galvanize', image: 'galvanize-climate-solution-website.png' },
-  { title: 'Forge Point', image: 'forgepoint-capitol-website.png' },
-  { title: 'Playground', image: 'playground-global-website.png' },
-  { title: 'IMC Licensing', image: 'imc-licensing-website.png' },
-  // { title: 'Wax Communications', image: 'wax-communications-website.jpg' },
-  { title: 'Telegram', image: 'telegram-website.jpg' },
-  { title: 'Parklands', image: 'the-parklands-website.png' },
+  {
+    title: 'Schedule It',
+    image: 'schedule-it-website.jpg',
+    url: 'https://schedule-it.prolificdigital.com/',
+  },
+  {
+    title: 'Goldhouse',
+    image: 'goldhouse-website.jpg',
+    url: 'https://goldhouse.prolificdigital.com/',
+  },
+  {
+    title: 'Frost Brown Todd',
+    image: 'frost-brown-todd-website.png',
+    url: 'https://frostbrowntodd.com/',
+  },
+  {
+    title: 'Elegance Living',
+    image: 'elegance-living-website.png',
+    url: 'https://elegance-living.com/',
+  },
+  {
+    title: 'Galvanize',
+    image: 'galvanize-climate-solution-website.png',
+    url: 'https://galvanizeclimatesolutions.com/',
+  },
+  {
+    title: 'Forge Point',
+    image: 'forgepoint-capitol-website.png',
+    url: 'https://forgepointcap.com/',
+  },
+  {
+    title: 'Playground',
+    image: 'playground-global-website.png',
+    url: 'https://playground.global/',
+  },
+  {
+    title: 'IMC Licensing',
+    image: 'imc-licensing-website.png',
+    url: 'https://imclicensing.com/',
+  },
+  // {
+  //   title: 'Wax Communications',
+  //   image: 'wax-communications-website.jpg',
+  //   url: 'https://waxcom.com/',
+  // },
+  {
+    title: 'Telegram',
+    image: 'telegram-website.jpg',
+    url: 'https://telegram.prolificdigital.com/',
+  },
+  {
+    title: 'Parklands',
+    image: 'the-parklands-website.png',
+    url: 'https://theparklands.org/',
+  },
   {
     title: 'Park Community Credit Union',
     image: 'park-community-credit-union--website.png',
+    url: 'https://parkcommunity.com/',
   },
   {
     title: 'New Directions Housing',
     image: 'new-direction-housing-website.png',
+    url: 'https://www.ndhc.org/',
   },
-  { title: 'Moonshine University', image: 'moonshine-university-website.png' },
-  { title: 'NBA2Lou', image: 'nba2lou-website.png' },
-  { title: 'MCM CPA', image: 'mcm-cpa-website.png' },
+  {
+    title: 'Moonshine University',
+    image: 'moonshine-university-website.png',
+    url: 'https://moonshineuniversity.com/',
+  },
+  {
+    title: 'NBA2Lou',
+    image: 'nba2lou-website.png',
+    url: 'https://nba2lou.com/',
+  },
+  {
+    title: 'MCM CPA',
+    image: 'mcm-cpa-website.png',
+    url: 'https://mcmcpa.com/',
+  },
   {
     title: 'Home of the Innocents',
     image: 'home-of-the-innocents-website.png',
+    url: 'https://www.homeoftheinnocents.org/',
   },
   {
     title: 'National Travel Association',
     image: 'national-travel-association-website.png',
+    url: 'https://ntaonline.com/',
   },
   // {
   //   title: 'International Coaching Federation',
   //   image: 'old-forester-website.jpeg',
+  //   url: 'https://coachingfederation.org/',
   // },
-  { title: 'Gravely', image: 'gravely-brewing-website.png' },
-  { title: 'Flavorman', image: 'flavorman-website.png' },
+  {
+    title: 'Gravely',
+    image: 'gravely-brewing-website.png',
+    url: 'https://www.gravelybrewing.com/',
+  },
+  {
+    title: 'Flavorman',
+    image: 'flavorman-website.png',
+    url: 'https://flavorman.com/',
+  },
   {
     title: 'Embroiderers Guild of America',
     image: 'egausa-website.png',
+    url: 'https://egausa.org/',
   },
-  { title: 'Carnegie Center', image: 'carnegie-center-website.png' },
+  {
+    title: 'Carnegie Center',
+    image: 'carnegie-center-website.png',
+    url: 'http://www.carnegiecenter.org/',
+  },
   { title: 'Belle of Louisville', image: 'belle-of-louisville-website.png' },
-  { title: 'BDAS', image: 'bdas-website.png' },
-  { title: 'APH Museum', image: 'aph-museum-website.png' },
+  { title: 'BDAS', image: 'bdas-website.png', url: 'https://bdastesting.com/' },
+  {
+    title: 'APH Museum',
+    image: 'aph-museum-website.png',
+    url: 'https://www.aph.org/the-museum-of-the-american-printing-house-for-the-blind-attracting-visitors-from-around-the-world/',
+  },
   {
     title: 'American Founders Bank',
     image: 'american-founders-bank-website.png',
   },
-  { title: 'Investors Heritage', image: 'investors-heritage-website.png' },
+  {
+    title: 'Investors Heritage',
+    image: 'investors-heritage-website.png',
+    url: 'https://investorsheritage.com/',
+  },
   { title: 'Funtown Mountain', image: 'funtown-mountain-website.png' },
-  { title: 'Globetrotters', image: 'globetrotters-website.png' },
-  { title: 'Hosparus Health', image: 'hosparus-health-website.png' },
+  {
+    title: 'Globetrotters',
+    image: 'globetrotters-website.png',
+    url: 'https://www.gec-group.com/',
+  },
+  {
+    title: 'Hosparus Health',
+    image: 'hosparus-health-website.png',
+    url: 'https://hosparushealth.org/',
+  },
 ];
 
 export default function Home() {
@@ -164,7 +266,12 @@ export default function Home() {
                     </div>
                   </Tilt>
                   <div className='content'>
-                    <h2 className='h6'>{item.title}</h2>
+                    <h2 className='title h6'>{item.title}</h2>
+                    {item.url && (
+                      <a href={item.url} target='_blank'>
+                        Visit site
+                      </a>
+                    )}
                   </div>
                 </div>
               ))}
